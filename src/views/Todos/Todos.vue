96227fab4b08de8169e16e5e23afe70587a80664
Miguel Santos <enium.miguel@gmail.com>
2023-12-11 07:59:42 -0300

    Update

--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,15 +1,35 @@
 <template>
-    <div>
-        <h1>Todos</h1>
-    </div>
+    <h1>Todos list</h1>
+
+    <ul>
+        <li v-for="todo in todos" :key="todo.identify">
+            {{ todo.name }}
+        </li>
+    </ul>
 </template>
 
 <script>
 export default {
-    name:"TodoView"
+    name: 'TodosVue',
+    setup() {
+        const todos = [
+            {identify: 1, name: 'tarefa 01', completed: true},
+            {identify: 2, name: 'tarefa 02', completed: false},
+            {identify: 3, name: 'tarefa 03', completed: true},
+        ]
+
+        const name = ref('default value')
+
+        name.value = 'teste 2'
+        
+        return {
+            todos,
+            name
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    
 </style>
